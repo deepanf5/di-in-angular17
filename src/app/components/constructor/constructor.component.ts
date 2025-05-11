39495d810e514bed637abab87187e9d35d68f08f
deepan <deepanappu1999@gmail.com>
2025-05-11 08:22:20 +0530

    adding constructor comp with DI

---
+++ src/app/components/constructor/constructor.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { LoggerService } from '../../services/logger.service';
+
+@Component({
+  selector: 'app-constructor',
+  standalone: true,
+  imports: [],
+  templateUrl: './constructor.component.html',
+  styleUrl: './constructor.component.css'
+})
+export class ConstructorComponent {
+
+  constructor(private loggerService:LoggerService){
+    this.loggerService.logger("constructor component is initalized")
+  }
+
+}
